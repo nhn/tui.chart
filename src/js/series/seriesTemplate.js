e7f148c01e4aad17a726cb264ddcf2c5ce9b2b60
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2015-09-21 21:23:58 +0900

    Remove unused code.

--- src/js/series/seriesTemplate.js
+++ src/js/series/seriesTemplate.js
@@ -7,13 +7,11 @@
 var templateMaker = require('../helpers/templateMaker.js');
 
 var tags = {
-    HTML_SERIES_LABEL_AREA: '<div class="ne-chart-series-label-area">{{ html }}</div>',
     HTML_SERIES_LABEL: '<div class="ne-chart-series-label" style="{{ cssText }}" data-group-index="{{ groupIndex }}" data-index="{{ index }}">{{ value }}</div>',
     TEXT_CSS_POSITION: 'left:{{ left }}px;top:{{ top }}px;font-family:{{ fontFamily }};font-size:{{ fontSize }}px'
 };
 
 module.exports = {
-    TPL_SERIES_LABEL_AREA: templateMaker.template(tags.HTML_SERIES_LABEL_AREA),
     TPL_SERIES_LABEL: templateMaker.template(tags.HTML_SERIES_LABEL),
     TPL_CSS_POSITION: templateMaker.template(tags.TEXT_CSS_POSITION)
 };
