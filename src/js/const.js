91ab1e895549e2f5b6c8571a6f6aec6957629b4c
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2015-10-29 12:50:50 +0900

    Update README.md, const.js

--- src/js/const.js
+++ src/js/const.js
@@ -1,28 +1,42 @@
 /**
- * @fileoverview chart const
- * @readonly
- * @enum {number}
+ * @fileoverview Chart const
  * @author NHN Ent.
  *         FE Development Team <dl_javascript@nhnent.com>
  */
 
-module.exports = {
-    /** chart id prefix */
+/**
+ * Chart const
+ * @readonly
+ * @enum {number}
+ */
+var chartConst = {
+    /** chart id prefix
+     * @type {string}
+     */
     CHAR_ID_PREFIX: 'tui-chart',
-    /** tooltip id prefix*/
+    /** tooltip id prefix
+     * @type {string}
+     */
     TOOLTIP_ID_PREFIX: 'tui-chart-tooltip',
-    /** chart types */
+    /** chart types
+     * @type {string}
+     */
     CHART_TYPE_BAR: 'bar',
+    /** @type {string} */
     CHART_TYPE_COLUMN: 'column',
+    /** @type {string} */
     CHART_TYPE_LINE: 'line',
+    /** @type {string} */
     CHART_TYPE_AREA: 'area',
+    /** @type {string} */
     CHART_TYPE_COMBO: 'combo',
+    /** @type {string} */
     CHART_TYPE_PIE: 'pie',
     /** chart padding */
     CHART_PADDING: 10,
     /** chart default width */
     CHART_DEFAULT_WIDTH: 500,
-    /** chart default hdiehgt */
+    /** chart default height */
     CHART_DEFAULT_HEIGHT: 400,
     /** hidden width */
     HIDDEN_WIDTH: 1,
@@ -38,26 +52,38 @@ module.exports = {
     DEFAULT_AXIS_TITLE_FONT_SIZE: 10,
     /** default font size of label */
     DEFAULT_LABEL_FONT_SIZE: 12,
-    /** default font size of series lable */
+    /** default font size of series label */
     DEFAULT_SERIES_LABEL_FONT_SIZE: 11,
-    /** default graph plugin */
+    /** @type {string} */
+    /** default graph plugin
+     * @type {string}
+     */
     DEFAULT_PLUGIN: 'raphael',
-    /** default tick color */
+    /** default tick color
+     * @type {string}
+     */
     DEFAULT_TICK_COLOR: 'black',
-    /** default theme name */
+    /** default theme name
+     * @type {string}
+     */
     DEFAULT_THEME_NAME: 'default',
-    /** stacked option types */
+    /** stacked option types
+     * @type {string}
+     */
     STACKED_NORMAL_TYPE: 'normal',
+    /** @type {string} */
     STACKED_PERCENT_TYPE: 'percent',
     /** empty axis label */
     EMPTY_AXIS_LABEL: '',
-    /** angel 360 */
+    /** angel */
     ANGLE_85: 85,
     ANGLE_90: 90,
     ANGLE_360: 360,
     /** radian */
     RAD: Math.PI / 180,
-    /** series legend types */
+    /** series legend types
+     * @type {string}
+     */
     SERIES_LEGEND_TYPE_OUTER: 'outer',
     /** series outer label padding */
     SERIES_OUTER_LABEL_PADDING: 20,
@@ -65,9 +91,13 @@ module.exports = {
     PIE_GRAPH_DEFAULT_RATE: 0.8,
     /** small rate of pie graph */
     PIE_GRAPH_SMALL_RATE: 0.65,
-    /** yAxis properties */
+    /** yAxis properties
+     * @type {array.<string>}
+     */
     YAXIS_PROPS: ['tickColor', 'title', 'label'], // yaxis theme의 속성 - chart type filtering할 때 사용됨
-    /** series properties */
+    /** series properties
+     * @type {array.<string>}
+     */
     SERIES_PROPS: ['label', 'colors', 'borderColor', 'singleColors'], // series theme의 속성 - chart type filtering할 때 사용됨
     /** title area width padding */
     TITLE_AREA_WIDTH_PADDING: 20,
@@ -75,13 +105,17 @@ module.exports = {
     XAXIS_LABEL_TOP_MARGIN: 10,
     /** right padding of vertical label */
     V_LABEL_RIGHT_PADDING: 10,
-    /** tooltip prefix */
+    /** tooltip prefix
+     * @type {string}
+     */
     TOOLTIP_PREFIX: 'tui-chart-tooltip',
     /** minimum pixel type step size */
     MIN_PIXEL_TYPE_STEP_SIZE: 40,
     /** maximum pixel type step size */
     MAX_PIXEL_TYPE_STEP_SIZE: 60,
-    /* tick info of percent stacked option */
+    /** tick info of percent stacked option
+     * @type {object}
+     */
     PERCENT_STACKED_TICK_INFO: {
         scale: {
             min: 0,
@@ -115,12 +149,19 @@ module.exports = {
     LINE_MARGIN_TOP: 5,
     /** tooltip gap */
     TOOLTIP_GAP: 5,
-    /** tooltip direction */
+    /** tooltip direction
+     * @type {string}
+     */
     TOOLTIP_DIRECTION_FORWORD: 'forword',
+    /** @type {string} */
     TOOLTIP_DIRECTION_BACKWORD: 'backword',
-    /** tooltip default position option */
+    /** tooltip default position option
+     * @type {string}
+     */
     TOOLTIP_DEFAULT_POSITION_OPTION: 'center top',
+    /** @type {string} */
     TOOLTIP_DEFAULT_HORIZONTAL_POSITION_OPTION: 'right middle',
     /** hide delay */
     HIDE_DELAY: 200
 };
+module.exports = chartConst;
