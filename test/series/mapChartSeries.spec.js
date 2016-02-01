6428887333d9f43b6ce03a72d45e193b23d0643f
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2016-02-02 08:13:02 +0900

    fix: Fix wrong file included.

--- test/series/mapChartSeries.spec.js
+++ test/series/mapChartSeries.spec.js
@@ -6,7 +6,7 @@
 
 'use strict';
 
-var MapChartSeries = require('../../src/js/series/MapChartSeries.js');
+var MapChartSeries = require('../../src/js/series/mapChartSeries.js');
 
 describe('MapChartSeries', function() {
     var series, dataProcessor, boundsMaker;
