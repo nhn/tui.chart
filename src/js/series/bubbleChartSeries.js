fe0ffedc47040ff2eb1c0126437e670d2bb8c460
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2016-04-19 15:14:09 +0900

    feat: Add  codes for making position for rendering of series area.

--- src/js/series/bubbleChartSeries.js
+++ src/js/series/bubbleChartSeries.js
@@ -6,7 +6,8 @@
 
 'use strict';
 
-var Series = require('./series');
+var Series = require('./series'),
+    chartConst = require('../const');
 
 var BubbleChartSeries = tui.util.defineClass(Series, /** @lends BubbleChartSeries.prototype */ {
     /**
@@ -19,14 +20,85 @@ var BubbleChartSeries = tui.util.defineClass(Series, /** @lends BubbleChartSerie
     },
 
     /**
-     * Make bounds of bubble chart.
-     * @returns {Array.<Array.<object>>} bounds
+     * Calculate step value for label axis.
+     * @returns {number}
      * @private
      */
-    _makeBounds: function() {
-        return [];
+    _calculateStep: function() {
+        var step = 0;
+        var dimension, seriesDataModel, size, len;
+
+        if (this.dataProcessor.hasCategories()) {
+            dimension = this.boundsMaker.getDimension('series');
+            seriesDataModel = this.dataProcessor.getSeriesDataModel(this.chartType);
+            len = this.dataProcessor.getCategories().length;
+
+            if (seriesDataModel.isGreaterXCountThanYCount()) {
+                size = dimension.width;
+            } else {
+                size = dimension.height;
+            }
+
+            step = size / len;
+        }
+
+        return step;
+    },
+
+    /**
+     * Make position for bubble chart.
+     * @param {number} positionByStep - position value by step
+     * @param {SeriesItemForCoordinateType} seriesItem - SeriesItemForCoordinateType
+     * @returns {{left: number, top: number}}
+     * @private
+     */
+    _makePosition: function(positionByStep, seriesItem) {
+        var dimension = this.boundsMaker.getDimension('series');
+        var ratioMap = seriesItem.ratioMap;
+        var left = tui.util.isExisty(ratioMap.x) ? ratioMap.x * dimension.width : positionByStep;
+        var top = tui.util.isExisty(ratioMap.y) ? ratioMap.y * dimension.height : positionByStep;
+        var padding = chartConst.SERIES_EXPAND_SIZE;
+
+        return {
+            left: padding + left,
+            top: padding + dimension.height - top
+        };
+    },
+
+    /**
+     * Make positions for bubble chart.
+     * @returns {Array.<Array.<{left: number, top: number}>>} positions
+     * @private
+     */
+    _makePositions: function() {
+        var self = this;
+        var seriesDataModel = this.dataProcessor.getSeriesDataModel(this.chartType);
+        var step = this._calculateStep();
+        var start = step ? step / 2 : 0;
+
+        return seriesDataModel.map(function(seriesGroup, index) {
+            var positionByStep = start + (step * index);
+            var makePosition = tui.util.bind(self._makePosition, self, positionByStep);
+
+            return seriesGroup.map(makePosition);
+        });
+    },
+
+    /**
+     * Make series data.
+     * @returns {{
+     *      groupPositions: Array.<Array.<{left: number, top: number}>>,
+     *      seriesDataModel: SeriesDataModel
+     * }} series data
+     * @private
+     * @override
+     */
+    _makeSeriesData: function() {
+        return {
+            groupPositions: this._makePositions(),
+            seriesDataModel: this.dataProcessor.getSeriesDataModel(this.chartType)
+        };
     }
 });
-
 
 module.exports = BubbleChartSeries;
