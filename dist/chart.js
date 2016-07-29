4fadf9f10a0db3258f2c6f39b7c1596ae489db14
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2016-07-29 12:27:44 +0900

    docs: Add banner

--- dist/chart.js
+++ dist/chart.js
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview tui.chart
+ * @author NHN Ent. FE Development Lab <dl_javascript@nhnent.com>
+ * @version 2.2.0
+ * @license MIT
+ * @link https://github.com/nhnent/tui.chart
+ */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * @fileoverview  Axis component.
@@ -29200,4 +29207,4 @@ module.exports = {
     tplMapChartDefault: templateMaker.template(htmls.HTML_MAP_CHART_DEFAULT_TEMPLATE)
 };
 
-},{"../helpers/templateMaker":64}]},{},[3,74])
+},{"../helpers/templateMaker":64}]},{},[3,74]);
