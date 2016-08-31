ae07e93702a811c23993a4219f74f36f103bca09
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2016-08-31 10:43:18 +0900

    test: Add tc for public api and user event

---
+++ test/userEvents/barChart.userEvent.spec.js
@@ -0,0 +1,189 @@
+/**
+ * @fileoverview Test user events for bar chart.
+ * @author NHN Ent.
+ *         FE Development Lab <dl_javascript@nhnent.com>
+ */
+
+'use strict';
+
+var dom = require('../../src/js/helpers/domHandler');
+
+describe('Test user events for bar chart', function() {
+    var rawData = {
+        categories: ['June', 'July', 'Aug', 'Sep', 'Oct', 'Nov'],
+        series: [
+            {
+                name: 'Budget',
+                data: [5000, 3000, 5000, 7000, 6000, 4000]
+            },
+            {
+                name: 'Income',
+                data: [8000, 1000, 7000, 2000, 5000, 3000]
+
+            },
+            {
+                name: 'Expenses',
+                data: [4000, 4000, 6000, 3000, 4000, 5000]
+            },
+            {
+                name: 'Debt',
+                data: [6000, 3000, 3000, 1000, 2000, 4000]
+            }
+        ]
+    };
+    var barChart;
+
+    beforeEach(function() {
+        var container = dom.create('DIV');
+
+        barChart = tui.chart.barChart(container, rawData, {
+            series: {
+                allowSelect: true
+            }
+        });
+    });
+
+    describe('load', function() {
+        it('load chart', function(done) {
+            barChart.on('load', done);
+        });
+    });
+
+    describe('selectSeries', function() {
+        it('select series', function(done) {
+            var customEvent = barChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            barChart.on('selectSeries', function(info) {
+                expect(info.chartType).toBe('bar');
+                expect(info.legend).toBe('Income');
+                expect(info.legendIndex).toBe(1);
+                expect(info.index).toBe(0);
+
+                done();
+            });
+
+            customEvent._onClick({
+                clientX: 100,
+                clientY: 100
+            });
+        });
+    });
+
+    describe('unselectSeries', function() {
+        it('unselect series', function(done) {
+            var customEvent = barChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            barChart.on('unselectSeries', function(info) {
+                expect(info.chartType).toBe('bar');
+                expect(info.legend).toBe('Income');
+                expect(info.legendIndex).toBe(1);
+                expect(info.index).toBe(0);
+
+                done();
+            });
+
+            // select
+            customEvent._onClick({
+                clientX: 100,
+                clientY: 100
+            });
+
+            setTimeout(function() {
+                // unselect
+                customEvent._onClick({
+                    clientX: 100,
+                    clientY: 100
+                });
+            });
+        });
+    });
+
+    describe('selectLegend', function() {
+        it('select legend', function(done) {
+            var legend = barChart.componentManager.get('legend');
+
+            barChart.on('selectLegend', function(info) {
+                expect(info).toEqual({
+                    chartType: 'bar',
+                    legend: 'Expenses',
+                    index: 2
+                });
+
+                done();
+            });
+
+            legend._selectLegend(2);
+        });
+    });
+
+    describe('beforeShowTooltip', function() {
+        it('before show tooltip', function(done) {
+            var customEvent = barChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            barChart.on('beforeShowTooltip', function(info) {
+                expect(info.chartType).toBe('bar');
+                expect(info.legend).toBe('Income');
+                expect(info.legendIndex).toBe(1);
+                expect(info.index).toBe(0);
+
+                done();
+            });
+
+            customEvent._onMousemove({
+                clientX: 100,
+                clientY: 100
+            });
+        });
+    });
+
+    describe('afterShowTooltip', function() {
+        it('after show tooltip', function(done) {
+            var customEvent = barChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            barChart.on('afterShowTooltip', function(info) {
+                expect(info.chartType).toBe('bar');
+                expect(info.legend).toBe('Income');
+                expect(info.legendIndex).toBe(1);
+                expect(info.index).toBe(0);
+                expect(info.element).toBeDefined();
+                expect(info.position.left).toBeDefined(true);
+                expect(info.position.top).toBeDefined(true);
+
+                done();
+            });
+
+            customEvent._onMousemove({
+                clientX: 100,
+                clientY: 100
+            });
+        });
+    });
+});
---
+++ test/userEvents/comboChart.userEvent.spec.js
@@ -0,0 +1,180 @@
+/**
+ * @fileoverview Test user events for line chart.
+ * @author NHN Ent.
+ *         FE Development Lab <dl_javascript@nhnent.com>
+ */
+
+'use strict';
+
+var dom = require('../../src/js/helpers/domHandler');
+
+describe('Test user events for combo chart', function() {
+    var rawData = {
+        categories: ['Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct'],
+        series: {
+            column: [
+                {
+                    name: 'Seoul',
+                    data: [11.3, 17.0, 21.0, 24.4, 25.2, 20.4, 13.9]
+                },
+                {
+                    name: 'NewYork',
+                    data: [9.9, 16.0, 21.2, 24.2, 23.2, 19.4, 13.3]
+
+                },
+
+                {
+                    name: 'Sydney',
+                    data: [18.3, 15.2, 12.8, 11.8, 13.0, 15.2, 17.6]
+                },
+                {
+                    name: 'Moskva',
+                    data: [4.4, 12.2, 16.3, 18.5, 16.7, 10.9, 4.2]
+                }
+            ],
+            line: [
+                {
+                    name: 'Average',
+                    data: [11, 15.1, 17.8, 19.7, 19.5, 16.5, 12.3]
+                }
+            ]
+        }
+    };
+    var comboChart;
+
+    beforeEach(function() {
+        var container = dom.create('DIV');
+
+        dom.append(document.body, container);
+
+        comboChart = tui.chart.comboChart(container, rawData, {
+            series: {
+                allowSelect: true
+            },
+            tooltip: {
+                grouped: true
+            }
+        });
+    });
+
+    describe('selectSeries', function() {
+        it('select series', function(done) {
+            var customEvent = comboChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            comboChart.on('selectSeries', function(info) {
+                expect(info.chartType).toBe('column');
+                expect(info.legend).toBe('Sydney');
+                expect(info.legendIndex).toBe(2);
+                expect(info.index).toBe(1);
+
+                done();
+            });
+
+            customEvent._onClick({
+                clientX: 123,
+                clientY: 330
+            });
+        });
+    });
+
+    describe('unselectSeries', function() {
+        it('unselect series', function(done) {
+            var customEvent = comboChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            comboChart.on('unselectSeries', function(info) {
+                expect(info.chartType).toBe('column');
+                expect(info.legend).toBe('Sydney');
+                expect(info.legendIndex).toBe(2);
+                expect(info.index).toBe(1);
+
+                done();
+            });
+
+            // select
+            customEvent._onClick({
+                clientX: 123,
+                clientY: 330
+            });
+
+            setTimeout(function() {
+                // unselect
+                customEvent._onClick({
+                    clientX: 123,
+                    clientY: 330
+                });
+            });
+        });
+    });
+
+    // group tooltip 부터
+    describe('beforeShowTooltip', function() {
+        it('before show tooltip', function(done) {
+            var customEvent = comboChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            comboChart.on('beforeShowTooltip', function(info) {
+                expect(info.chartType).toBe('combo');
+                expect(info.index).toBe(1);
+                expect(info.range.start).toBeDefined();
+                expect(info.range.end).toBeDefined();
+
+                done();
+            });
+
+            customEvent._onMousemove({
+                clientX: 100,
+                clientY: 100
+            });
+        });
+    });
+
+    describe('afterShowTooltip', function() {
+        it('after show tooltip', function(done) {
+            var customEvent = comboChart.componentManager.get('customEvent');
+
+            spyOn(customEvent, '_getContainerBound').and.returnValue({
+                left: 50,
+                top: 80,
+                right: 450,
+                bottom: 380
+            });
+
+            comboChart.on('afterShowTooltip', function(info) {
+                expect(info.chartType).toBe('combo');
+                expect(info.index).toBe(1);
+                expect(info.range.start).toBeDefined();
+                expect(info.range.end).toBeDefined();
+                expect(info.element).toBeDefined();
+                expect(info.position.left).toBeDefined(true);
+                expect(info.position.top).toBeDefined(true);
+
+                done();
+            });
+
+            customEvent._onMousemove({
+                clientX: 100,
+                clientY: 100
+            });
+        });
+    });
+});
