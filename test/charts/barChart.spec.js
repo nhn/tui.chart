fa4906d2796fdadeed88c826bbab92fd9d06e616
김진우 <jw.kim@nhnent.com>
2018-06-26 19:07:19 +0900

    fixed barChart test

--- test/charts/barChart.spec.js
+++ test/charts/barChart.spec.js
@@ -11,7 +11,6 @@ describe('Test for BarChart', () => {
 
     describe('constructor()', () => {
         beforeEach(() => {
-            // spyOn(BarChart.prototype, '_updateOptionsRelatedDiverging');
             spyOn(BarChart.prototype, '_initializeOptions');
             spyOn(BarChart.prototype, '_createComponentManager').and.returnValue({
                 register: jasmine.createSpy('register')
@@ -23,24 +22,7 @@ describe('Test for BarChart', () => {
             barInstance = new BarChart({
                 categories: ['cate1', 'cate2', 'cate3'],
                 series: {
-                    'chartType': [
-                        {
-                            name: 'Legend1',
-                            data: [20, 30, 50]
-                        },
-                        {
-                            name: 'Legend2',
-                            data: [40, 40, 60]
-                        },
-                        {
-                            name: 'Legend3',
-                            data: [60, 50, 10]
-                        },
-                        {
-                            name: 'Legend4',
-                            data: [80, 10, 70]
-                        }
-                    ]
+                    'chartType': []
                 }
             }, {
                 title: {
