f8ec825808b039f5407b9a8bb65be379303ecfa7
김진우 <35218826+jinwoo-kim-nhn@users.noreply.github.com>
2018-11-17 14:06:46 +0900

    fix: legend calculate width (#146)
    * fixed bug calculate legend width
    
    * fixed legend width caculate
    
    * changed naming
    
    * fixed holizontal legend height
    
    * apply codereview
    
    * apply codereview

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,7 @@ module.exports = {
   },
   'rules': {
       'prefer-destructuring': ['error', {
-          VariableDeclarator: {array: true, object: true},
-          AssignmentExpression: {array: false, object: false}
+          VariableDeclarator: {array: true, object: true}
       }]
   }
 };
