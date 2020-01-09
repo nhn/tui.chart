395482d8806f8a20ab2e1eea88ae759b73f8a3ca
jung-han <hanjung@nhnent.com>
2020-01-09 11:09:40 +0900

    chore: remove new line

--- src/js/models/scaleData/scaleDataModel.js
+++ src/js/models/scaleData/scaleDataModel.js
@@ -21,6 +21,7 @@ class ScaleDataModel {
         this.theme = params.theme;
         this.hasRightYAxis = !!params.hasRightYAxis;
         this.prevValidLabelCount = null;
+
         this.initScaleData(params.addedDataCount);
         this.initForAutoTickInterval();
     }
