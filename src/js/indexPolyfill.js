8eee09397a127fc40b6e5050cd6a0e284aa9e25a
jung-han <hanjung@nhnent.com>
2020-02-20 17:19:59 +0900

    chore: apply core-js

---
+++ src/js/indexPolyfill.js
@@ -0,0 +1,20 @@
+import 'core-js/features/object/create';
+import 'core-js/features/object/assign';
+import 'core-js/features/object/entries';
+import 'core-js/features/object/values';
+import 'core-js/features/object/keys';
+
+import 'core-js/features/array/for-each';
+import 'core-js/features/array/is-array';
+import 'core-js/features/array/filter';
+import 'core-js/features/array/reduce';
+import 'core-js/features/array/from';
+import 'core-js/features/array/map';
+import 'core-js/features/array/fill';
+
+import 'core-js/features/json/stringify';
+import 'core-js/features/function/bind';
+
+import chart from './index';
+
+module.exports = chart;
--- test/index.js
+++ test/index.js
@@ -1,3 +1,19 @@
-require('babel-polyfill');
+require('core-js/features/object/create');
+require('core-js/features/object/assign');
+require('core-js/features/object/entries');
+require('core-js/features/object/values');
+require('core-js/features/object/keys');
+
+require('core-js/features/array/for-each');
+require('core-js/features/array/is-array');
+require('core-js/features/array/filter');
+require('core-js/features/array/reduce');
+require('core-js/features/array/from');
+require('core-js/features/array/map');
+require('core-js/features/array/fill');
+
+require('core-js/features/json/stringify');
+require('core-js/features/function/bind');
+
 const testsContext = require.context('.', true, /spec.js$/);
 testsContext.keys().forEach(testsContext);
