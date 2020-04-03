2fc2cd032a0d5efc2bb94f7bac52b21904c58d70
jung-han <hanjung@nhnent.com>
2020-04-03 09:19:15 +0900

    fix: broken test
    mock dataProcessor options

--- test/components/tooltips/groupTooltip.spec.js
+++ test/components/tooltips/groupTooltip.spec.js
@@ -21,7 +21,8 @@ describe('GroupTooltip', () => {
       'getLegendData',
       'getLegendItem',
       'getCategoryCount',
-      'makeTooltipCategory'
+      'makeTooltipCategory',
+      'options'
     ]);
   });
 
