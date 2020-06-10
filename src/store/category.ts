7f556a5ba31d49776907242c26254a1f3226543b
shiren <shiren@MacBook-Pro.local>
2020-06-10 16:44:23 +0900

    refactor: extract category module and some tcs corrected

---
+++ src/store/category.ts
@@ -0,0 +1,26 @@
+import { StoreModule, SeriesRaw } from '@t/store/store';
+
+import { sortCategories } from '@src/helpers/utils';
+
+function makeCategories(series: SeriesRaw) {
+  const categories: Set<string> = new Set();
+
+  Object.keys(series).forEach((key) => {
+    series[key].forEach(({ data }) => {
+      data.forEach((datum) => {
+        categories.add(Array.isArray(datum) ? String(datum[0]) : String(datum.x));
+      });
+    });
+  });
+
+  return Array.from(categories).sort(sortCategories);
+}
+
+const category: StoreModule = {
+  name: 'category',
+  state: ({ series, categories }) => ({
+    categories: categories ? categories : makeCategories(series),
+  }),
+};
+
+export default category;
---
+++ tests/store/category.spec.ts
@@ -0,0 +1,64 @@
+import category from '@src/store/category';
+import { StateFunc } from '@t/store/store';
+
+describe('Category Store', () => {
+  describe('state', () => {
+    it('should make categories from param', () => {
+      const state = (category.state as StateFunc)({
+        categories: ['a', 'b', 'c'],
+        options: {},
+        series: {},
+      });
+
+      expect(state.categories).toEqual(['a', 'b', 'c']);
+    });
+
+    it('should make categories with coordinate data', () => {
+      let state = (category.state as StateFunc)({
+        options: {
+          chart: {
+            width: 1,
+            height: 2,
+          },
+        },
+        series: {
+          line: [
+            {
+              name: 'test',
+              data: [
+                { x: 10, y: 5 },
+                { x: 1, y: 2 },
+                { x: 3, y: 5 },
+              ],
+            },
+          ],
+        },
+      });
+
+      expect(state.categories).toEqual(['1', '3', '10']);
+
+      state = (category.state as StateFunc)({
+        options: {
+          chart: {
+            width: 1,
+            height: 2,
+          },
+        },
+        series: {
+          line: [
+            {
+              name: 'test',
+              data: [
+                [10, 5],
+                [1, 2],
+                [3, 5],
+              ],
+            },
+          ],
+        },
+      });
+
+      expect(state.categories).toEqual(['1', '3', '10']);
+    });
+  });
+});
