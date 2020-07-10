bca463afd72089e024d58e3f750b8a02fbd84600
jung-han <hanjung@nhnent.com>
2020-07-10 13:58:43 +0900

    fix: datalabel undefined

--- src/store/dataLabels.ts
+++ src/store/dataLabels.ts
@@ -135,7 +135,7 @@ function makePointLabelInfo(point: PointDataLabel, dataLabelOptions: DataLabelOp
   return {
     x: x + offsetX,
     y: y + offsetY,
-    text: formatter!(point.value!),
+    text: formatter(point.value!),
     textAlign: 'center',
     textBaseline,
     style,
@@ -428,7 +428,7 @@ function makeRectLabelInfo(rect: RectDataLabel, dataLabelOptions: DataLabelOptio
 
   return {
     ...labelPosition,
-    text: formatter!(value!),
+    text: formatter(value!),
     style: type === 'stackTotal' ? stackTotal?.style! : style,
   };
 }
@@ -453,7 +453,7 @@ const dataLabels: StoreModule = {
         const labelOptions = getDataLabelsOptions(dataLabelOptions, type, withStack);
         const disableStackTotal = type === 'stackTotal' && !labelOptions.stackTotal?.visible;
 
-        if (disableStackTotal) {
+        if (disableStackTotal || !model.value) {
           return;
         }
 
