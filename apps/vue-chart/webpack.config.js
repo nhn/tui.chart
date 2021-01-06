bb8e65db4109701ff8aace1775cca972b8134849
jung-han <hanjung@nhnent.com>
2021-01-06 16:44:23 +0900

    chore: add vue wrapper

---
+++ apps/vue-chart/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'toastui-vue-chart.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'toastui',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js',
+    },
+  },
+  externals: {
+    'tui-chart': {
+      commonjs: 'tui-chart',
+      commonjs2: 'tui-chart',
+      amd: 'tui-chart',
+      root: ['tui', 'chart'],
+    },
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
