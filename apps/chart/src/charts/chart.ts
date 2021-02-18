9c1a062f180e39a6bd88eafdece72529acd48389
한정 <hanjung@nhn.com>
2021-02-18 15:03:16 +0900

    fix: ie10 bugs (#584)
    * fix: set export menu position using chart width instead of container width
    
    * fix: resizeObserver event detect ie10
    
    * fix: change position call using container size for update options

--- apps/chart/src/charts/chart.ts
+++ apps/chart/src/charts/chart.ts
@@ -237,13 +237,15 @@ export default abstract class Chart<T extends Options> {
     const { usingContainerSize } = this.store.state;
 
     if (
-      (usingContainerSize.height && !this.el.style.height) ||
-      (usingContainerSize.width && !this.el.style.width)
+      (usingContainerSize.height && !this.el.style.height.length) ||
+      (usingContainerSize.width && !this.el.style.width.length)
     ) {
       throw new Error(message.AUTO_LAYOUT_CONTAINER_SIZE_ERROR);
     }
 
-    if (isUndefined(ResizeObserver)) {
+    const isResizeObserverAPIExist = typeof ResizeObserver === 'undefined';
+
+    if (isResizeObserverAPIExist) {
       window.addEventListener('resize', this.debounceWindowResizeEvent);
     } else {
       this.resizeObserver = new ResizeObserver((entries) => {
@@ -576,7 +578,7 @@ export default abstract class Chart<T extends Options> {
    */
   public resize = (size: Partial<Size>) => {
     this.resetSeries();
-    this.store.dispatch('updateOptions', { options: { chart: { ...size } } });
+    this.dispatchOptionsEvent('updateOptions', { chart: { ...size } });
   };
 
   /**
@@ -610,16 +612,16 @@ export default abstract class Chart<T extends Options> {
     const isAutoWidth = isAutoValue(width);
     const isAutoHeight = isAutoValue(height);
 
+    this.store.dispatch(
+      'setUsingContainerSize',
+      getUsingContainerSize(eventName, usingContainerSize, width, height)
+    );
+
     if ((usingContainerWidth || usingContainerHeight) && isNumber(width) && isNumber(height)) {
       this.clearResizeEvent();
     } else if (!(usingContainerWidth || usingContainerHeight) && (isAutoWidth || isAutoHeight)) {
       this.setResizeEvent();
     }
-
-    this.store.dispatch(
-      'setUsingContainerSize',
-      getUsingContainerSize(eventName, usingContainerSize, width, height)
-    );
   };
 
   protected dispatchOptionsEvent(eventName: 'initOptions' | 'updateOptions', options: Options) {
