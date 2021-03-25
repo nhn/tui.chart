115dcf29b8a9bf98744276a299287dc78bc1c07b
jung-han <hanjung@nhnent.com>
2021-03-25 15:19:57 +0900

    fix: use export default for vue wrapper

--- apps/vue-chart/src/index.js
+++ apps/vue-chart/src/index.js
@@ -1,6 +1,24 @@
-import { chartType, createComponent } from './base';
+import { createComponent } from './base';
 
-export const {
+const areaChart = createComponent('area');
+const barChart = createComponent('bar');
+const boxPlotChart = createComponent('boxPlot');
+const bubbleChart = createComponent('bubble');
+const bulletChart = createComponent('bullet');
+const columnChart = createComponent('column');
+const columnLineChart = createComponent('columnLine');
+const heatmapChart = createComponent('heatmap');
+const lineChart = createComponent('line');
+const lineAreaChart = createComponent('lineArea');
+const lineScatterChart = createComponent('lineScatter');
+const nestedPieChart = createComponent('nestedPie');
+const pieChart = createComponent('pie');
+const radarChart = createComponent('radar');
+const scatterChart = createComponent('scatter');
+const treemapChart = createComponent('treemap');
+const radialBarChart = createComponent('radialBar');
+
+export {
   areaChart,
   barChart,
   boxPlotChart,
@@ -17,8 +35,5 @@ export const {
   radarChart,
   scatterChart,
   treemapChart,
-} = chartType.reduce((obj, chartName) => {
-  obj[`${chartName}Chart`] = createComponent(chartName);
-
-  return obj;
-}, {});
+  radialBarChart,
+};
