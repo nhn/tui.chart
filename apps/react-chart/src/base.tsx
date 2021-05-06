00429520fcd7d7714fe7dd5cb40e2c752fc00b36
jung-han <hanjung@nhn.com>
2021-05-06 16:36:11 +0900

    refactor: change react js file to ts file, add snowpack config

--- apps/react-chart/src/base.tsx
+++ apps/react-chart/src/base.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import Chart from '@toast-ui/chart';
-// import { ChartType, Options } from '@toast-ui/chart/types/store/store';
-// import { Chart } from '@toast-ui/chart/types';
+import Chart, { CustomEventType } from '@toast-ui/chart';
+import { ChartInstanceType, ChartProps, ChartType, ChartPropsType } from '../index';
 
-type ChartFactoryType = 'nestedPie' | 'lineScatter' | 'lineArea' | 'columnLine';
-
-// type 지정 필요
-interface ChartProps {
-  style: Record<string, string>;
-  data: any;
-  options: any;
-}
-
-export default function (chartType: ChartFactoryType) {
-  return class ChartFactory extends React.Component<ChartProps> {
+export default function <T extends ChartPropsType>(chartType: ChartType) {
+  return class ChartFactory extends React.Component<ChartProps<T>> {
     rootEl = React.createRef<HTMLDivElement>();
 
-    chartInst: Chart | null = null;
+    chartInst: ChartInstanceType | null = null;
 
     getRootElement() {
       return this.rootEl.current;
@@ -26,16 +16,16 @@ export default function (chartType: ChartFactoryType) {
       return this.chartInst;
     }
 
-    bindEventHandlers(props: ChartProps, prevProps?: ChartProps) {
+    bindEventHandlers(props: ChartProps<T>, prevProps?: ChartProps<T>) {
       Object.keys(props)
         .filter((key) => /on[A-Z][a-zA-Z]+/.test(key))
         .forEach((key) => {
-          // const eventName = (key[2].toLowerCase() + key.slice(3)) as CustomEventType;
+          const eventName = (key[2].toLowerCase() + key.slice(3)) as CustomEventType;
           if (prevProps && prevProps[key] !== props[key]) {
             // @TODO: need to implement
             // this.chartInst?.off(eventName);
           }
-          // this.chartInst?.on(eventName, this.props[key]);
+          this.chartInst?.on(eventName, this.props[key]);
         });
     }
 
@@ -43,18 +33,18 @@ export default function (chartType: ChartFactoryType) {
       const { data, options } = this.props;
       this.chartInst = Chart[`${chartType}Chart`]({ el: this.rootEl.current, data, options });
 
-      this.bindEventHandlers(this.props);
+      this.bindEventHandlers(this.props as ChartProps<T>);
     }
 
-    shouldComponentUpdate(nextProps) {
+    shouldComponentUpdate(nextProps: ChartProps<T>) {
       const currentData = this.props.data;
-      const nextData = nextProps.data;
+      const nextData = nextProps.data as any;
 
       if (currentData !== nextData) {
-        // this.getInstance()?.setData(nextData);
+        this.getInstance()?.setData(nextData);
       }
 
-      this.bindEventHandlers(nextProps, this.props);
+      this.bindEventHandlers(nextProps, this.props as ChartProps<T>);
 
       return false;
     }
