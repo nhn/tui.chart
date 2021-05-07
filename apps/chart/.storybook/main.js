5f47ad83d9745c337edd26ae2cdddd20c604c91e
jung-han <hanjung@nhn.com>
2021-05-07 15:55:17 +0900

    chore: add storybook addon

--- apps/chart/.storybook/main.js
+++ apps/chart/.storybook/main.js
@@ -3,7 +3,7 @@ const devWebpackConfig = require('../webpack.config.js')({}, { mode: 'developmen
 
 module.exports = {
   stories: ['@stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-knobs'],
   core: {
     builder: 'webpack5',
   },
