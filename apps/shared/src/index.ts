f578d2a23376bddc1e21849ab25009697dc28f0b
jung-han <hanjung@nhn.com>
2021-05-25 17:20:41 +0900

    feat: add color, scale helper code to shared

--- apps/shared/src/index.ts
+++ apps/shared/src/index.ts
@@ -1,8 +1,13 @@
 import * as utils from './helpers/utils';
 import * as reactive from './store/reactive';
+import * as scale from './helpers/scale';
+import * as color from './helpers/color';
 import EventEmitter from './helpers/eventEmitter';
+
 export default {
   ...utils,
   ...reactive,
+  ...scale,
+  ...color,
   EventEmitter,
 };
